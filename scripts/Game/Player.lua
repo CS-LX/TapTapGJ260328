f8224030424bf15bd9814f7d9f28c8c13b4516ba
Maker <maker@taptap.com>
2026-03-29 13:59:43 +0800

    audio: 操作音效(左移右移跳跃下蹲)音量改为50%

--- scripts/Game/Player.lua
+++ scripts/Game/Player.lua
@@ -180,7 +180,7 @@ function Player.SwitchLane(direction)
     if newLane >= -1 and newLane <= 1 then
         State.currentLane = newLane
         State.targetLaneX = State.currentLane * Config.LANE_WIDTH
-        SFX.Play("player_lane_switch.ogg")
+        SFX.Play("player_lane_switch.ogg", 0.5)
     end
 end
 
@@ -188,7 +188,7 @@ function Player.Jump()
     if not State.isJumping and not State.isSliding then
         State.isJumping = true
         State.playerVelocityY = Config.JUMP_VELOCITY
-        SFX.Play("throw.ogg")
+        SFX.Play("throw.ogg", 0.5)
     end
 end
 
@@ -196,7 +196,7 @@ function Player.Slide()
     if not State.isJumping and not State.isSliding then
         State.isSliding = true
         State.slideTimer = Config.SLIDE_DURATION
-        SFX.Play("player_slide.ogg")
+        SFX.Play("player_slide.ogg", 0.5)
     end
 end
 
